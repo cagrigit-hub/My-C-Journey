9e999844528d61a85277b3a18845ae3d0ee8b78d
Çağrı Kutay Ok <77447064+cagrigit-hub@users.noreply.github.com>
2024-02-22 20:21:57 +0300

    next-improvs

--- pointers/struct_pointers.c
+++ pointers/struct_pointers.c
@@ -7,6 +7,7 @@ typedef struct {
   int day;
 } date;
 
+void printArr(date*, int);
 
 int main() {
     date *today;
@@ -22,17 +23,29 @@ int main() {
     printf("the date is %d/%d/%d\n", today->day, today->month, today->year);
     free(today);
 
+    date todayX;
+    todayX.day = 4;
+    todayX.month =5;
+    todayX.year = 1999;
+
+    printf("the date is %d/%d/%d\n", today->day, today->month, today->year);
+
+    date weekArr[2]= {
+        {
+            1,1,1996
+        },
+         {
+            1,1,2000
+         }
+    };
+    printArr(weekArr,2);
+    printf("------------------------\n");
 
     date *weekDays;
     weekDays = (date*)malloc(sizeof(date) * 7);
     int i;
     for (i = 0; i < 7; i++) {
-        date day = {
-            i,
-            1,
-            1996
-        };
-        weekDays[i] = day;
+        weekDays[i].day = 4;
     }
     for (i = 0; i < 7; i++) {
         date today =  weekDays[i];
@@ -42,4 +55,13 @@ int main() {
     
     free(weekDays);
     return 0;
+}
+
+
+void printArr(date* arr, int size) {
+    int i;
+    for (i = 0 ; i< size; i++) {
+        date today = arr[i];
+        printf("the date is %d/%d/%d\n", today.day, today.month, today.year);
+    }
 }
